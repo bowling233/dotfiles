f7f5f69073764a1685187096d9e46c721970b244
Bowling <zhubaolin228@gmail.com>
2023-03-17 01:04:41 +0800

    modified install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,14 @@ sudo apt install python3 python3-pip python-is-python3
 sudo apt install build-essential cmake vim-nox python3-dev
 sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
 pip install --user cmake
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --all
-vim -c ':PlugInstall' -c ':PlugStatus' -c 'qa!'
+cd ~/.vim/plugged/YouCompleteMe
+python3 install.py --clangd-completer
+vim -c ':PlugInstall!' -c ':PlugStatus' -c 'qa!'
 
 echo '+ Installing oh-my-zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo '+ Installing powerlevel10k'
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sudo apt-get install fonts-powerline
 echo '+ Installing zsh-autosuggestions'
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
