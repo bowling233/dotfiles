607fb56921e278763826ce42e382825e06fa9309
bowling233 <zhubaolin228@gmail.com>
2024-11-09 05:09:30 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -39,9 +39,12 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
         echo "Installing oh-my-zsh"
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git \
+git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull \
+  || git clone https://gitee.com/romkatv/powerlevel10k.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone --depth=1 https://github.com/jeffreytse/zsh-vi-mode \
+git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode pull \
+  || git clone https://github.com/jeffreytse/zsh-vi-mode \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions \
+git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull \
+  || git clone https://github.com/zsh-users/zsh-autosuggestions \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
