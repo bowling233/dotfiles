1b409dfd0109ba9043824a1161a997db02fa6730
bowling233 <zhubaolin228@gmail.com>
2025-02-27 20:00:31 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,7 @@ done
 case "$OSTYPE" in
 linux-gnu*)
 	if ! getent passwd "$(whoami)" | grep -q "$(which zsh)"; then
-		echo "+ Changing default shell to zsh"
+		print_info "+ Changing default shell to zsh"
 		chsh -s "$(which zsh)"
 	fi
 	;;
@@ -67,33 +67,33 @@ esac
 
 # oh-my-zsh, themes, plugins
 if [ ! -e ~/.oh-my-zsh/oh-my-zsh.sh ]; then
-	echo "+ Installing oh-my-zsh"
+	print_info "+ Installing oh-my-zsh"
 	rm -rf ~/.oh-my-zsh
 	sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended
 fi
 
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-	echo "+ Installing powerlevel10k"
+	print_info "+ Installing powerlevel10k"
 	git clone https://gitee.com/romkatv/powerlevel10k.git \
 		"${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode" ]; then
-	echo "+ Installing zsh-vi-mode"
+	print_info "+ Installing zsh-vi-mode"
 	git clone https://github.com/jeffreytse/zsh-vi-mode \
 		"${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode"
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-	echo "+ Installing zsh-autosuggestions"
+	print_info "+ Installing zsh-autosuggestions"
 	git clone https://github.com/zsh-users/zsh-autosuggestions \
 		"${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-	echo "+ Installing zsh-syntax-highlighting"
+	print_info "+ Installing zsh-syntax-highlighting"
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 		"${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autoupdate" ]; then
-	echo "+ Installing autoupdate"
+	print_info "+ Installing autoupdate"
 	git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins \
 		"${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autoupdate"
 fi
@@ -117,20 +117,20 @@ DST=(
 
 for i in "${!SRC[@]}"; do
 	if [ -e "${DST[$i]}" ] && [ ! -L "${DST[$i]}" ]; then
-		echo "File ${DST[$i]} exists, moving to ${DST[$i]}.bak"
+		print_warning "File ${DST[$i]} exists, moving to ${DST[$i]}.bak"
 		mv "${DST[$i]}" "${DST[$i]}".bak
 	fi
 	# if [ ! -d "$(dirname "${DST[$i]}")" ]; then
-	# 	echo "Creating directory $(dirname "${DST[$i]}")"
+	# 	print_info "Creating directory $(dirname "${DST[$i]}")"
 	# 	mkdir -p "$(dirname "${DST[$i]}")"
 	# fi
-	echo "+ Linking ${SRC[$i]} to ${DST[$i]}"
+	print_info "+ Linking ${SRC[$i]} to ${DST[$i]}"
 	ln -sf "${SRC[$i]}" "${DST[$i]}"
 done
 
 # add crontab to git -C "$SCRIPT_DIR" pull
 if ! crontab -l | grep -q "git -C $SCRIPT_DIR pull"; then
-	echo "+ Adding crontab to git pull"
+	print_info "+ Adding crontab to git pull"
 	(
 		crontab -l 2>/dev/null
 		echo "0 0 * * * git -C $SCRIPT_DIR pull"
