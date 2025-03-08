c459290310fb435626324651121ba79d1f487edc
bowling233 <zhubaolin228@gmail.com>
2025-03-08 10:46:12 +0800

    update

--- srv/wireguard/host.sh
+++ srv/wireguard/host.sh
@@ -4,11 +4,16 @@ set -x
 case $1 in
   "up")
     wg-quick up config/wg_confs/wg0.conf
+    # systemctl enable wg-quick@wg0.service
+    # systemctl start wg-quick@wg0.service
     ;;
   "down")
     wg-quick down config/wg_confs/wg0.conf
+    # systemctl disable wg-quick@wg0.service
+    # systemctl stop wg-quick@wg0.service
     ;;
   *)
     echo "Invalid argument"
     ;;
 esac
+
